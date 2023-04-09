9554345fed12ca0225188586c0b4d8c288be2c5c
zbur5 <sakula39562@gmail.com>
2023-04-09 17:58:41 +0900

    1758

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const myImage = document.querySelector('img')
 
 myImage.onclick = () => {
     const mySrc = myImage.getAttribute('src');
-    if (mySrc = 'images/img-blue.png'){
+    if (mySrc === 'images/img-blue.png'){
         myImage.setAttribute('src','images/img-purple.png');
     }else{
         myImage.setAttribute('src','images/img-blue.png');
